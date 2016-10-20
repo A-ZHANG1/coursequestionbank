457ce26effbec2f9b40f411ceedccd84775ded14
hrzlvn <hrzlvn@gmail.com>
2016-10-19 18:46:55 -0700

    rollback to previous login setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Coursequestionbank::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  #OmniAuth.config.test_mode = true
+  OmniAuth.config.test_mode = true
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
