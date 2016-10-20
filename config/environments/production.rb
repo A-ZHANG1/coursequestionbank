234ec7ee5357dd7af321a9f143840706519a6367
hrzlvn <hrzlvn@gmail.com>
2016-10-20 07:41:55 -0700

    fixed production environment config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,8 @@ Coursequestionbank::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-<<<<<<< HEAD
   OmniAuth.config.test_mode = true
 
-=======
->>>>>>> 2fdeda9c5b01d0c24f72f4186a3547d5f6c4be11
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
