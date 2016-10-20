f02570d7441fc7fe9ff39323caa5894d49ac2450
hrzlvn <hrzlvn@gmail.com>
2016-10-19 18:45:08 -0700

    rollback to previous login setting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :github, Figaro.env.github_key!, Figaro.env.github_secret!
-  provider :developer, :fields => [:uid], :uid_field => [:uid]
-  if Rails.env.development? or Rails.env.test? 
+  provider :github, Figaro.env.github_key!, Figaro.env.github_secret!
+  if Rails.env.development? or Rails.env.test?
     provider :developer, :fields => [:uid], :uid_field => [:uid]
     # see seeds.rb for available :developer logins
     SessionController.skip_before_filter :verify_authenticity_token, only: :create # to avoid problems when testing with SSL turned on with OmniAuth Developer strategy
