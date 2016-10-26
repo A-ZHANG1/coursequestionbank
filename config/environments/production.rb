537b462b53b7293d4d216efa819f29fcc32535c8
hrzlvn <hrzlvn@gmail.com>
2016-10-25 21:27:33 -0700

    turn off Oauth developer mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,8 @@ Coursequestionbank::Application.configure do
   OmniAuth.config.test_mode = true
 
   # etc.
-})
+end
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-end
