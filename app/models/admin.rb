af9e606cc9ecc7439704319eb1c34de208f37856
hrzlvn <hrzlvn@gmail.com>
2016-10-27 00:09:34 -0700

    fixed instructors MVC, instrcutor accessibility and visibility done

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,7 @@
 class Admin < Instructor
-
+  has_many :collections
+  has_many :problems
+  attr_accessible :collections
+  attr_accessible :problems
+  @privilege = "Admin"
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,7 @@
 class Instructor < User
   has_many :collections
+  has_many :problems
   attr_accessible :collections
+  attr_accessible :problems
   @privilege = "Instructor"
 end
