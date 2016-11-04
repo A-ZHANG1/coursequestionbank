430a678d86d6304a643d282e521de339e9d15f47
harryww@berkeley.edu <harrytflv@gmail.com>
2016-11-04 00:44:23 -0700

    Have working checkboxes

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -216,3 +216,32 @@ var ChangeCollections = {
   }
 };
 $(ChangeCollections.setup);
+
+var ChangeCollectionsByCheckbox = {
+  setup: function() {
+    $('.collection-checkbox').each(function() {
+      var container = $(this);
+      container.click(function() {
+        $.ajax({
+          url: $(this).attr('action'),
+          type: 'PUT',
+          data: {"collection": $(this).attr("collection")}
+        });
+
+        // var button = $(this).find('input[type="submit"]');
+        //
+        // if (button.hasClass('btn-info')) {
+        //   button.removeClass('btn-info');
+        //   button.addClass('btn-default');
+        // }
+        // else if (button.hasClass('btn-default')) {
+        //   button.removeClass('btn-default');
+        //   button.addClass('btn-info');
+        // }
+
+        return true;
+      });
+    });
+  }
+};
+$(ChangeCollectionsByCheckbox.setup);
