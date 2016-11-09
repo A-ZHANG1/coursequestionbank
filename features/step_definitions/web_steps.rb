6d7d0f9ea6fff576ade69e96a09e79e035ba05fd
Harry Zeng <harrytflv@gmail.com>
2016-11-09 09:43:41 +0000

    Added new test case for admin panel, and modifed the view so that it can be tested

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -431,3 +431,7 @@ end
 Then(/^I should see "(.*?)" under "(.*?)"$/) do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
+
+Given(/^there exist a user with username "(.*?)" and uid "(.*?)"$/) do |username, uid|
+  Instructor.find_by_username_and_uid(username, uid).should_not be nil
+end
