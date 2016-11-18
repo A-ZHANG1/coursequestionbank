73a4d8afffdbe85699bcb0b427a7c7186a3d21cd
hrzlvn <hrzlvn@gmail.com>
2016-11-17 17:20:19 -0800

    refactor problems.js

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -1,14 +1,23 @@
-
-
-
-
-// buttons = ["collection", "bloom"]
-
-// buttons.each do |button|
-//   $("#{button}").click(function(){
-//     $("#button}").hide()
-
-//   })
+var MinorUpdate = {
+    setup: function() {
+        $('.minor_form form').submit(function(e) {
+            $.ajax({
+                context: this,
+                url: $(this).attr('action'),
+                type: 'POST',
+                data: $(this).serialize(),
+                success: function(data, textStatus, jqXHR) {
+                    if (data.error === null)
+                        window.location.reload();
+                    else
+                        $(this).find('.message').text(data.error);
+                }
+            });
+            return false;
+        });
+    }
+};
+$(MinorUpdate.setup);
 
 
 var AdditionalHidden = {
@@ -16,48 +25,18 @@ var AdditionalHidden = {
   setup: function() {
     $('.additional').each(function() {
       var problem = $(this);
-      
-      function toggle_behavior(button_name){
-        problem.find(button_name).toggle();
-      }
-
-      function hide_behavior(button_name){
-        problem.find(button_name).hide();
-      }
-
-      function show_behavior(button_name){
-        problem.find(button_name).show();
-      }
-
-      function event_handling(select_button, hide_button_one, hide_button_two, toggle_button){
-        problem.find(select_button).click(function() {
-        toggle_behavior(toggle_button);
-        hide_behavior(hide_button_one);
-        hide_behavior(hide_button_two);
+      problem.find('.supersede_button').click(function() {
+        problem.find('.supersede_form').toggle();
+        problem.find('.history_list').hide();
+          problem.find('.minor_form').hide();
         return false;
-      });}
-
-      event_handling('.supersede_button', '.history_list', '.minor_form', '.supersede_form');
-      // problem.find('.supersede_button').click(function() {
-      //   // problem.find('.supersede_form').toggle();
-      //   // problem.find('.history_list').hide();
-      //   // problem.find('.minor_form').hide();
-      //   toggle_behavior('.supersede_form');
-      //   hide_behavior('.history_list');
-      //   hide_behavior('.minor_form');
-      //   return false;
-      // });
-
-      event_handling('.history_list', '.supersede_form', '.minor_form', '.history_list');
-      // problem.find('.history_button').click(function() {
-      //   // problem.find('.supersede_form').hide();
-      //   // problem.find('.history_list').toggle();
-      //   // problem.find('.minor_form').hide();
-      //   hide_behavior('.supersede_form');
-      //   toggle_behavior('.history_list');
-      //   hide_behavior('minor_form');
-      //   return false;
-      // });
+      });
+      problem.find('.history_button').click(function() {
+        problem.find('.supersede_form').hide();
+        problem.find('.history_list').toggle();
+          problem.find('.minor_form').hide();
+        return false;
+      });
 
         problem.find('.btn-update').click(function() {
             console.log("Received");
@@ -69,33 +48,25 @@ var AdditionalHidden = {
             return false;
         });
 
-        event_handling('.minorupdate_button', '.supersede_form', '.history_list', '.minor_form');
-        // problem.find('.minorupdate_button').click(function() {
-        //     // problem.find('.minor_form').toggle();
-        //     // problem.find('.supersede_form').hide();
-        //     // problem.find('.history_list').hide();
-        //     toggle_behavior('.minor_form');
-        //     hide_behavior('.supersede_form');
-        //     hide_behavior('.history_list');
-        //     return false;
-        // });
+        problem.find('.minorupdate_button').click(function() {
+            problem.find('.minor_form').toggle();
+            problem.find('.supersede_form').hide();
+            problem.find('.history_list').hide();
+            return false;
+        });
 
 
 
       problem.find('.hide_checkbox').click(function() {
-        // problem.find('.edit-Collections').show()
-        // problem.find('.show_checkbox').show()
-        show_behavior('.edit-Collections');
-        show_behavior('.show_checkbox');
+        problem.find('.edit-Collections').show()
+        problem.find('.show_checkbox').show()
         $(this).hide();
         return false;
       });
 
       problem.find('.show_checkbox').click(function() {
-        // problem.find('.edit-Collections').hide()
-        // problem.find('.hide_checkbox').show()
-        hide_behavior('.edit-Collections');
-        show_behavior('.hide_checkbox');
+        problem.find('.edit-Collections').hide()
+        problem.find('.hide_checkbox').show()
         $(this).hide();
         return false;
       });
@@ -106,8 +77,8 @@ var AdditionalHidden = {
           $(this).show();
         })
         $(this).hide();
-        //problem.find('.collections-less-toggle').show()
-        show_behavior('.collections-less-toggle');
+        problem.find('.collections-less-toggle').show()
+
         return false;
       })
 
@@ -116,16 +87,14 @@ var AdditionalHidden = {
           $(this).hide();
         })
         $(this).hide();
-        //problem.find('.collections-more-toggle').show()
-        show_behavior('.collections-more-toggle');
+        problem.find('.collections-more-toggle').show()
         return false;
       })
 
 
       var hide_collections_button = problem.find('button.collections-less-toggle')
       hide_collections_button.click(function(){
-        //problem.find('.collection-button btn').toggle();
-        toggle_behavior('.collections-button btn');
+        problem.find('.collection-button btn').toggle();
         return false;
       })
 
