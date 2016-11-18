44a8f09c4e8cb7ae0332fbee25b9bea6be2c97f2
hrzlvn <hrzlvn@gmail.com>
2016-11-17 19:11:42 -0800

    further improve design pattern

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -1,16 +1,3 @@
-
-
-
-
-// buttons = ["collection", "bloom"]
-
-// buttons.each do |button|
-//   $("#{button}").click(function(){
-//     $("#button}").hide()
-
-//   })
-
-
 var AdditionalHidden = {
 
   setup: function() {
@@ -38,26 +25,8 @@ var AdditionalHidden = {
       });}
 
       event_handling('.supersede_button', '.history_list', '.minor_form', '.supersede_form');
-      // problem.find('.supersede_button').click(function() {
-      //   // problem.find('.supersede_form').toggle();
-      //   // problem.find('.history_list').hide();
-      //   // problem.find('.minor_form').hide();
-      //   toggle_behavior('.supersede_form');
-      //   hide_behavior('.history_list');
-      //   hide_behavior('.minor_form');
-      //   return false;
-      // });
-
       event_handling('.history_list', '.supersede_form', '.minor_form', '.history_list');
-      // problem.find('.history_button').click(function() {
-      //   // problem.find('.supersede_form').hide();
-      //   // problem.find('.history_list').toggle();
-      //   // problem.find('.minor_form').hide();
-      //   hide_behavior('.supersede_form');
-      //   toggle_behavior('.history_list');
-      //   hide_behavior('minor_form');
-      //   return false;
-      // });
+      event_handling('.minorupdate_button', '.supersede_form', '.history_list', '.minor_form');
 
         problem.find('.btn-update').click(function() {
             console.log("Received");
@@ -68,19 +37,6 @@ var AdditionalHidden = {
             });
             return false;
         });
-
-        event_handling('.minorupdate_button', '.supersede_form', '.history_list', '.minor_form');
-        // problem.find('.minorupdate_button').click(function() {
-        //     // problem.find('.minor_form').toggle();
-        //     // problem.find('.supersede_form').hide();
-        //     // problem.find('.history_list').hide();
-        //     toggle_behavior('.minor_form');
-        //     hide_behavior('.supersede_form');
-        //     hide_behavior('.history_list');
-        //     return false;
-        // });
-
-
 
       problem.find('.hide_checkbox').click(function() {
         // problem.find('.edit-Collections').show()
@@ -100,26 +56,26 @@ var AdditionalHidden = {
         return false;
       });
 
+      function toggle_collection(click_toggle, operate_toggle, show_toggle){
 
-      problem.find('button.collections-more-toggle').click(function(){
-        problem.find('.collection-button').each(function(){
-          $(this).show();
-        })
-        $(this).hide();
-        //problem.find('.collections-less-toggle').show()
-        show_behavior('.collections-less-toggle');
-        return false;
-      })
-
-      problem.find('button.collections-less-toggle').click(function(){
-        problem.find('.collection-button.btn-default').each(function(){
+          problem.find(click_toggle).click(function(){
+          problem.find(operate_toggle).each(function(){
+            if (operate_toggle == '.collection-button'){
+              $(this).show();
+            }
+            else if(click_toggle == '.collection-button.btn-default'){
+              $(this).hide();
+            }
+          })
           $(this).hide();
+          //problem.find('.collections-less-toggle').show()
+          show_behavior(show_toggle);
+          return false;
         })
-        $(this).hide();
-        //problem.find('.collections-more-toggle').show()
-        show_behavior('.collections-more-toggle');
-        return false;
-      })
+      }
+
+      toggle_collection('button.collections-more-toggle', '.collection-button', '.collections-less-toggle')
+      toggle_collection('button.collections-less-toggle', '.collection-button.btn-default', '.collections-more-toggle')
 
 
       var hide_collections_button = problem.find('button.collections-less-toggle')
