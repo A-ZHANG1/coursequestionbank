6990c5dc081408023acc239b019a2d84b8663fd0
hrzlvn <hrzlvn@gmail.com>
2016-11-17 18:55:00 -0800

    fixed JS bugs that leads to no action

---
+++ app/assets/javascripts/changePrivacy.js
@@ -0,0 +1,17 @@
+var ChangePrivacy = {
+  setup: function() {
+    $('.prob_privacy').submit(function() {
+      var button = $(this).find('input[type="submit"]');
+      var newValue = button.attr('value') === 'Public' ? 'Private' : 'Public';
+      button.attr('value', newValue);
+      $(this).find('input[name="privacy"]').attr('value', button.attr('value'));
+      $.ajax({
+        url: $(this).attr('action'),
+        type: 'PUT',
+        data: $(this).serialize()
+      });
+      return false;
+    });
+  }
+};
+$(ChangePrivacy.setup);
