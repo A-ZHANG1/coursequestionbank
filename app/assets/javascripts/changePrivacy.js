9c95af54d16e4fc9afcb62b66d3f4509acab7021
hrzlvn <hrzlvn@gmail.com>
2016-11-17 17:23:00 -0800

    refactor problems.js

--- app/assets/javascripts/changePrivacy.js
+++ app/assets/javascripts/changePrivacy.js
@@ -1,17 +1,18 @@
-var ChangePrivacy = {
-  setup: function() {
-    $('.prob_privacy').submit(function() {
-      var button = $(this).find('input[type="submit"]');
-      var newValue = button.attr('value') === 'Public' ? 'Private' : 'Public';
-      button.attr('value', newValue);
-      $(this).find('input[name="privacy"]').attr('value', button.attr('value'));
-      $.ajax({
-        url: $(this).attr('action'),
-        type: 'PUT',
-        data: $(this).serialize()
-      });
-      return false;
-    });
-  }
-};
-$(ChangePrivacy.setup);
+// var ChangePrivacy = {
+//   setup: function() {
+//     $('.prob_privacy').submit(function() {
+//       var button = $(this).find('input[type="submit"]');
+//       var newValue = button.attr('value') === 'Public' ? 'Private' : 'Public';
+//       button.attr('value', newValue);
+//       $(this).find('input[name="privacy"]').attr('value', button.attr('value'));
+//       $.ajax({
+//         url: $(this).attr('action'),
+//         type: 'PUT',
+//         data: $(this).serialize()
+//       });
+//       return false;
+//     });
+//   }
+// };
+// $(ChangePrivacy.setup);
+
--- app/assets/javascripts/collectionHidden.js
+++ app/assets/javascripts/collectionHidden.js
@@ -1,21 +1,21 @@
-var collectionHidden = {
+// var collectionHidden = {
 
-	problem.find('.hide_checkbox').click(function() {
-        problem.find('.edit-Collections').show()
-        problem.find('.show_checkbox').show()
+// 	problem.find('.hide_checkbox').click(function() {
+//         problem.find('.edit-Collections').show()
+//         problem.find('.show_checkbox').show()
         
-        $(this).hide();
-        return false;
-      });
+//         $(this).hide();
+//         return false;
+//       });
 
-	problem.find('button.collections-more-toggle').click(function(){
-        problem.find('.collection-button').each(function(){
-          $(this).show();
-        })
-        $(this).hide();
-        problem.find('.collections-less-toggle').show()
+// 	problem.find('button.collections-more-toggle').click(function(){
+//         problem.find('.collection-button').each(function(){
+//           $(this).show();
+//         })
+//         $(this).hide();
+//         problem.find('.collections-less-toggle').show()
         
-        return false;
-      });
-};
-$(collectionHidden.setup);
+//         return false;
+//       });
+// };
+// $(collectionHidden.setup);
