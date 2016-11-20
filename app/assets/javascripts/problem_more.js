1f2378135b7fc9ebaa95995c4ccf3d1197c4bee6
Steven Wu <stevenwuyinze@berkeley.edu>
2016-11-19 16:39:27 -0800

    Funky Select on answering view JS fixed

--- app/assets/javascripts/problem_more.js
+++ app/assets/javascripts/problem_more.js
@@ -17,20 +17,23 @@ var Question = {
                         $(this).mouseleave();
                     });
                 }
-                var clickOnEntry = function() {
-                    choice = $(this).find(":checkbox")
-                    if (choice.is(':checked')) {
-                        choice.prop('checked', false);
+                var clickOnEntry = function(event) {
+                    target = $(event.target);
+                    checked = $(this).find(":checkbox").prop('checked');
+                    if (target.is(':checkbox')) {
+                        checked = !checked;
+                    }
+                    if (checked) {
                         $(this).on("mouseover", hoverOnEntry);
                         $(this).on("mouseleave", hoverOffEntry);
                     } else {
                         // unCheckAll($(this).parent());
                         $(this).mouseover();
-                        choice.prop('checked', true);
                         $(this).off("mouseover");
                         $(this).off("mouseleave");
                     }
-                    return false;
+                    choice.prop('checked', !checked);
+                    return;
                 }
 
                 var checkCorrect = function(checkButton) {
@@ -59,7 +62,7 @@ var Question = {
                 question.find(".entrybox").mouseover(hoverOnEntry);
                 question.find(".entrybox").mouseleave(hoverOffEntry);
                 question.find(".entrybox").click(clickOnEntry);
-                question.find(".check-answer").click(checkCorrect)
+                question.find(".check-answer").click(checkCorrect);
             });
 
         });
@@ -80,24 +83,27 @@ var Question = {
                         $(this).mouseleave();
                     });
                 }
-                var clickOnEntry = function() {
+                var clickOnEntry = function(event) {
+                    target = $(event.target);
                     choice = $(this).find(":radio");
-                    if (choice.is(':checked')) {
-                        choice.prop('checked', false);
+                    checked = $(this).find(":radio").prop('checked');
+                    if (target.is(':radio')) {
+                        checked = !checked;
+                    }
+                    if (checked) {
                         $(this).on("mouseover", hoverOnEntry);
                         $(this).on("mouseleave", hoverOffEntry);
                     } else {
                         unCheckAll($(this).parent());
                         $(this).mouseover();
-                        choice.prop('checked', true);
                         $(this).off("mouseover");
                         $(this).off("mouseleave");
                     }
+                    choice.prop('checked', !checked);
                     return true;
                 }
 
                 var checkCorrect = function(checkButton) {
-
                     $(this).parent().find(".entrybox").each(function() {
                         // debugger
                         // debugger
