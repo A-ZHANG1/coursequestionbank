405f57c8b3b1bbcc7fd254a7622889c9cd3467e9
yanxinyi <yanxinyi@gmail.com>
2016-11-29 14:14:51 -0800

    student answer table model changed

--- app/controllers/student_answer_controller.rb
+++ app/controllers/student_answer_controller.rb
@@ -1,16 +1,25 @@
 class StudentAnswerController < ApplicationController
   # :attempt, :correctness, :problem_uid, :last_used, :updated_at, :created_date
   def create
+
+    debugger
     # new answer
     answer = StudentAnswer.new(:attempt => params[:attempt],
                                :correctness => params[:correctness],
-                               :problem_uid => params[:problem_uid]
+                               :problem_uid => params[:problem_uid],
+                               :instructor_id => params[:instructor_id],
+                               :created_at => DateTime.current
     )
+    answer.save
 
     # add answer to problem
-    Problem.find(params[:problem_id]).student_answers << answer
-
-    # add answer to student
-    Instructor.find(params[:instructor_id]).student_answers << answer
+    # problem = Problem.find(params[:problem_id])
+    # problem.student_answers << answer
+    # problem.save
+    #
+    # # add answer to student
+    # instrucotr = Instructor.find(params[:instructor_id])
+    # instrucotr.student_answers << answer
+    # instrucotr.save
   end
 end
--- app/models/student_answer.rb
+++ app/models/student_answer.rb
@@ -1,5 +1,5 @@
 class StudentAnswer < ActiveRecord::Base
-  attr_accessible :attempt, :correctness, :problem_uid, :last_used, :updated_at, :created_date
+  attr_accessible :attempt, :correctness, :problem_uid, :last_used, :updated_at, :created_date, :instructor_id
   belongs_to :instructor
   belongs_to :question
 end
