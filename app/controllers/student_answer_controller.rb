d6f4e608dd147e8d9cc15ec2fec808cac3093a09
harryww@berkeley.edu <harrytflv@gmail.com>
2016-11-29 12:48:11 -0800

    To do: add a ajax call

--- app/controllers/student_answer_controller.rb
+++ app/controllers/student_answer_controller.rb
@@ -1,11 +1,16 @@
 class StudentAnswerController < ApplicationController
   # :attempt, :correctness, :problem_uid, :last_used, :updated_at, :created_date
   def create
+    # new answer
     answer = StudentAnswer.new(:attempt => params[:attempt],
                                :correctness => params[:correctness],
                                :problem_uid => params[:problem_uid]
     )
+
+    # add answer to problem
     Problem.find(params[:problem_id]).student_answers << answer
+
+    # add answer to student
     Instructor.find(params[:instructor_id]).student_answers << answer
   end
 end
